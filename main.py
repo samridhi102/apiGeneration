6bf5bc37700e56115f2ae796345e557d9d3ff98b
samridhi <samridhi102kri@gmail.com>
2023-04-13 17:18:14 +0530

    changes

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
-import numpy as np
-import flask
-import werkzeug
-from flask import Flask,request,jsonify
 import pickle
+from ctypes import util
+
+import cv2
+import flask
+import numpy as np
+import werkzeug
+from PIL import Image
+from flask import send_from_directory, request, render_template, jsonify
+import numpy
 import imageio
-model = pickle.load(open('X.pickle','rb'))
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return "Sam"
+import os
+from wsgiref.simple_server import make_server
+from gevent.pywsgi import WSGIServer
+from keras.preprocessing import image
+from sklearn.feature_extraction.text import TfidfVectorizer
+from tensorflow.python.keras import models
+
+app = flask.Flask(__name__)
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+dic = ['Gram Negative', 'Gram Positive']
+
+
+@app.route('/', methods=['GET', 'POST'])
+def welcome():
+    return "Welcome"
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/predict/', methods=['GET', 'POST'])
@@ -17,14 +40,23 @@ def handle_request():
     filename = werkzeug.utils.secure_filename(imagefile.filename)
     print("\nReceived image File name : " + imagefile.filename)
     imagefile.save(filename)
-
-    img = imageio.imread(filename, pilmode="L")
-    if img.shape != (28, 28):
-        return "Image size mismatch " + str(img.shape) + ". \nOnly (28, 28) is acceptable."
-    img = img.reshape(784)
-    predicted_label = np.argmax(model.predict(np.array([img]))[0], axis=-1)
+    img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    img = Image.fromarray(img, 'L')
+    img = img.resize((128, 128))
+    img = np.array(img)
+    print("Earlier: ", img.shape)
+    img = img.flatten()
+    img = img.reshape(1, -1)
+    print("Later: ", img.shape)
+    predicted_label = model.predict(img)
+    # predicted_label = np.argmax(model.predict(np.array([img]))[0], axis=1)
     print(predicted_label)
-    return str(predicted_label)
+    return dic[predicted_label[0]]
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=os.environ.get('PORT', 8081), debug=True)
+    # with make_server('', 5000, app) as server:
+    #     print('serving on port 5000...\nvisit http://127.0.0.1:5000\nTo exit press ctrl + c')
+    #     server.serve_forever()
+    # app.run(host="0.0.0.0", port=os.environ.get('PORT', 5000), debug=True)
