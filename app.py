4440b34ef263a591cffd5939569ba1c8842cb5df
samridhi <samridhi102kri@gmail.com>
2023-04-21 14:43:11 +0530

    changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pickle
 from ctypes import util
+from wsgiref.simple_server import make_server
 
 import cv2
 import flask
@@ -7,15 +8,7 @@ import numpy as np
 import werkzeug
 from PIL import Image
 from flask import send_from_directory, request, render_template, jsonify
-import numpy
-import imageio
 import os
-from wsgiref.simple_server import make_server
-from gevent.pywsgi import WSGIServer
-from keras.preprocessing import image
-from sklearn.feature_extraction.text import TfidfVectorizer
-from tensorflow.python.keras import models
-
 app = flask.Flask(__name__)
 
 model = pickle.load(open('model.pkl', 'rb'))
@@ -55,8 +48,5 @@ def handle_request():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=os.environ.get('PORT', 8081), debug=True)
-    # with make_server('', 5000, app) as server:
-    #     print('serving on port 5000...\nvisit http://127.0.0.1:5000\nTo exit press ctrl + c')
-    #     server.serve_forever()
-    # app.run(host="0.0.0.0", port=os.environ.get('PORT', 5000), debug=True)
+    # app.run(host="127.0.0.1", port=os.environ.get('PORT', 5000), debug=True)
+    app.run(host="0.0.0.0", port=os.environ.get('PORT', 8080), debug=True)
